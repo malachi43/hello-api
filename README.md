6862f5bec36d0659dd78b58ea9d2005ab8b88bcd
Malachi <ukomalachi@gmail.com>
2024-07-01 10:03:13 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## HELLO API
+Hello-api is just a basic web server, the returns the current temperature in celsius based on your current location.
+
+### RUNNING THE SERVER LOCALLY
+- To run the server locally you must have nodejs installed, you can download it [here](https://nodejs.org/en/download/package-manager)
+- In your terminal enter the command ```npm install``` to install the required dependencies.
+- Enter the command ```node index.js``` to start the development server.
+- Open your preferred browser and enter the url ```http://localhost:3000/api/hello?visitor_name=<your name>```
---
+++ index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const { AxiosError } = require("axios");
+const PORT = process.env.PORT || 3000;
+const baseUrl = `/api`
+const weatherApiBaseUrl = `http://api.weatherapi.com/v1`
+
+app.get(`${baseUrl}/hello`, async (req, res) => {
+    try {
+        const visitorName = req.query?.visitor_name;
+        const key = process.env.WEATHER_API_KEY
+        const { data } = await axios.get(`${weatherApiBaseUrl}/current.json`, {
+            params: {
+                key,
+                q: req.ip
+            }
+        })
+        const { location: { name } } = data;
+        const { current: { temp_c } } = data;
+
+        res.status(200).json({
+            client_ip: req.ip,
+            location: name,
+            greeting: `Hello, ${visitorName ? visitorName : "user"}!,the temperature is ${temp_c} degrees Celcius in ${name}`
+        })
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            return res.status(400).json({
+                status: false,
+                error_msg: error.response.data,
+            })
+        }
+        res.status(400).json({
+            status: false,
+            error_msg: error.message,
+        })
+    }
+
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port: ${PORT}. Press Ctrl+C to terminate.`)
+})
